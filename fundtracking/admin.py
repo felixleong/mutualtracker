28ed7d72c8ecd11f5d5d329b504dd36d2f16230e
Seh Hui "Felix" Leong <felixleong@gmail.com>
2011-01-01 22:09:33 +0800

    1. Added filtering and search to price admin page 2. Define columns for fund and price admins

--- fundtracking/admin.py
+++ fundtracking/admin.py
@@ -1,8 +1,17 @@
 from mutualtracker.fundtracking.models import Fund, Price, Report, Company, Industry, Country
 from django.contrib import admin
 
-admin.site.register(Fund)
-admin.site.register(Price)
+class FundAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name',)
+
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ('fund', 'date', 'nav',)
+    list_filter = ['date', ]
+    search_fields = ['fund__code', 'fund__name']
+
+# Add the admin modules
+admin.site.register(Fund, FundAdmin)
+admin.site.register(Price, PriceAdmin)
 admin.site.register(Report)
 admin.site.register(Company)
 admin.site.register(Industry)
